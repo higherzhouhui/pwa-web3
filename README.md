c47986ca67bab0a12ebc44b2e99408a819eac5b4
hc <zhouhui608@gmail.com>
2024-03-09 04:53:25 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# pwa-web3
+## PWA Lens Protocol example app
+
+A boilerplate combining [Lens Protocol](https://www.lens.xyz/), [WalletConnect](https://walletconnect.com/), [Privy](https://www.privy.io/), and [ShadCN UI](https://ui.shadcn.com/)
+
+![Lens PWA Screenshot](pwa-header.png)
+
+## Getting started
+
+1. Clone repo
+
+```sh
+git clone git@github.com:dabit3/lens-pwa.git
+```
+
+2. Install dependencies
+
+```sh
+npm install # or yarn, etc..
+```
+
+3. Configure environment variables for WalletConnect
+
+```sh
+# rename .example.env.local to .env.local 
+NEXT_PUBLIC_WC_ID=
+```
+
+4. Run the app
+
+```sh
+npm run dev
+```
---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import './globals.css'
+import { Web3ModalProvider } from "@/components/web3modal-provider"
+import { ThemeProvider } from "@/components/theme-provider"
+import { LensProvider } from "@/components/lens-provider"
+import { Nav } from "@/components/nav"
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/* PWA config */}
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" /> 
+      <meta name="apple-mobile-web-app-title" content="Lens PWA" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/images/icons/iconmain-512x512.png" />
+      <meta name="theme-color" content="#000000" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      <body>
+        <Web3ModalProvider>
+          <LensProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <Nav />
+              {children}
+            </ThemeProvider>
+          </LensProvider>
+        </Web3ModalProvider>
+      </body>
+    </html>
+  )
+}
+
