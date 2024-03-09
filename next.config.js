de203d77214ddb414a3daad7bee1411fbfa24dd6
hc <zhouhui608@gmail.com>
2024-03-09 14:52:54 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
